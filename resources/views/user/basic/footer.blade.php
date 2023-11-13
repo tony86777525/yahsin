99d78c41eb46aea47eb7b16fc563dd4bcb4cdd6b
DC130 <tony86777525@gmail.com>
2023-11-13 22:03:12 +0800

    add pay svg

--- resources/views/user/basic/footer.blade.php
+++ resources/views/user/basic/footer.blade.php
@@ -1,3 +1,4 @@
 <footer class="footer">
+    <div class="footer__pay"><img src="/img/img-support-cards.svg"></div>
     <div class="footer__copyright">{!! __('user.foot.copyright')  !!}</div>
 </footer>
